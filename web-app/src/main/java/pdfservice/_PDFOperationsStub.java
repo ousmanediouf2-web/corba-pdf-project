e5f70ecc263a54461d010320e97de027c0974d18
ousmanediouf2-web <ousmane.diouf2@etu.ussein.edu.sn>
2026-05-12 20:44:38 +0000

    Add files via upload

--- web-app/src/main/java/pdfservice/_PDFOperationsStub.java
+++ web-app/src/main/java/pdfservice/_PDFOperationsStub.java
@@ -12,12 +12,10 @@ public class _PDFOperationsStub extends org.omg.CORBA.portable.ObjectImpl implem
             int l = $in.read_long(); byte[] $result = new byte[l]; $in.read_octet_array($result, 0, l);
             return $result;
         } catch (org.omg.CORBA.portable.ApplicationException $ex) {
-            $in = $ex.getInputStream();
-            String _id = $ex.getId();
+            $in = $ex.getInputStream(); String _id = $ex.getId();
             if (_id.equals(PDFExceptionHelper.id())) throw PDFExceptionHelper.read($in);
             throw new org.omg.CORBA.MARSHAL(_id);
-        } catch (org.omg.CORBA.portable.RemarshalException $rm) {
-            return mergePDFs(pdfFiles);
+        } catch (org.omg.CORBA.portable.RemarshalException $rm) { return mergePDFs(pdfFiles);
         } finally { _releaseReply($in); }
     }
 
@@ -165,24 +163,5 @@ public class _PDFOperationsStub extends org.omg.CORBA.portable.ObjectImpl implem
     }
 
     private static String[] __ids = { "IDL:pdfservice/PDFOperations:1.0" };
-
     public String[] _ids() { return __ids; }
-
-    private void readObject(java.io.ObjectInputStream s) throws java.io.IOException {
-        String str = s.readUTF();
-        String[] args = null;
-        java.util.Properties props = null;
-        org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, props);
-        try { org.omg.CORBA.Object obj = orb.string_to_object(str);
-              org.omg.CORBA.portable.Delegate delegate = ((org.omg.CORBA.portable.ObjectImpl) obj)._get_delegate();
-              _set_delegate(delegate); }
-        finally { orb.destroy(); }
-    }
-
-    private void writeObject(java.io.ObjectOutputStream s) throws java.io.IOException {
-        String[] args = null; java.util.Properties props = null;
-        org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, props);
-        try { String str = orb.object_to_string(this); s.writeUTF(str); }
-        finally { orb.destroy(); }
-    }
 }
